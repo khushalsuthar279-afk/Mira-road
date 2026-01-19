f62b21ed8f8336ff09ba58b1f56ffc2232b547b8
khushalsuthar279-afk <khushalsuthar279@gmail.com>
2026-01-19 19:28:45 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+    const menu = document.getElementById("menu");
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
+}
+
+const student = {
+  fullName: "rahul",
+  age:18,
+  cgpa: 22,
+};
+
+console.log(student);
